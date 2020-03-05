eaedeb6785343df134f40379a1bd4ae0d547cacc
SoravitYK <42952008+SoravitYK@users.noreply.github.com>
2020-03-05 14:48:30 +0700

    update3

--- WorkWell/WorkWell/settings.py
+++ WorkWell/WorkWell/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'co_working_space'
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'WorkWell.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+                'default': {
+                    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                    'NAME': 'WorkWell',
+                    'USER': 'postgres',
+                    'PASSWORD': '1234',
+                    'HOST': 'localhost',
+                    'PORT': '',
+                }
+            }
 
 
 # Password validation
--- WorkWell/co_working_space/models.py
+++ WorkWell/co_working_space/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Member(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    money = models.IntegerField()
+
+class TopupLog(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    topup_date = models.DateTimeField(auto_now=True)
+    topup_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Zone(models.Model):
+    zone = (
+        ('GR', 'Green Zone'),
+        ('GO', 'Gold Zone'),
+        ('PR', 'Private Zone')
+    )
+
+    title = models.CharField(max_length=2, choices=zone)
+    description = models.TextField(null=True, blank=True)
+    price = models.IntegerField()
+
+class SeatBooking(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null = True)
+    total_price = models.IntegerField()
+    create_date = models.DateField(auto_now_add=True)
+    create_by = models.ForeignKey(User, on_delete=models.CASCADE)
