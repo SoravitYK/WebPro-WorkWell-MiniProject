36ae63239fa64a77b31e95ec708dec2a1c13d7bc
SoravitYK <42952008+SoravitYK@users.noreply.github.com>
2020-03-07 20:36:03 +0700

    update7

--- WorkWell/co_working_space/views.py
+++ WorkWell/co_working_space/views.py
@@ -2,10 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import authenticate, login, logout
-from django.db.models import Q
 from django.utils import timezone
-
-from datetime import datetime
 
 from co_working_space.models import Member, SeatBooking, TopupLog, Zone
 
@@ -15,15 +12,29 @@ from co_working_space.models import Member, SeatBooking, TopupLog, Zone
 def index(request):
     context = {}
     context["zone"] = Zone.zone
+    check_in = None
     if request.method == "GET":
         member_id = request.GET.get("member_id")
-        context["member_id"] = member_id
-        if member_id:
-            try:
-                member = Member.objects.get(pk=member_id)
-                context["member"] = member
-            except Member.DoesNotExist:
-                context["error"] = "Member ID doesn't exist"
+
+    if request.method == "POST":
+        member_id = request.POST.get("member_id")
+        check_in = request.POST.get("check_in")
+        zone = request.POST.get("zone")
+
+    context["member_id"] = member_id
+    if member_id:
+        try:
+            member = Member.objects.get(pk=member_id)
+            context["member"] = member
+            seat = SeatBooking.objects.filter(member=member)
+            context["seat"] = seat
+            if check_in:
+                zone_type = Zone.objects.get(title="GREEN")
+                if member.money >= zone_type.price:
+                    seat_booking = SeatBooking.objects.create(member=member, zone=zone_type, create_by=request.user)
+                    seat_booking.save()
+        except Member.DoesNotExist:
+            context["error"] = "Member ID doesn't exist"
     return render(request, template_name='co_working_space/index.html', context=context)
 
 def register(request):
